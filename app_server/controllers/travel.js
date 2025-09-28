edcd0614afcd9454a22bc356b8e2a4c5194c79c8
Brendan Clarke <Brendan.clarke@snhu.edu>
2025-09-28 10:46:46 -0700

    Module 4 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
+var fs = require('fs'); 
+var trips = JSON.parse(fs.readFileSync('./app_server/data/trips.json', 
+'utf8'));
+
 /* GEt Travel view */
 const travel = (req, res) => {
-  res.render('travel', { title: 'Travlr Travel' });
+  res.render('travel', { title: 'Travlr Getaways', trips });
 };
 
 module.exports = {
